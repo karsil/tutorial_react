14536170a50eda246a6a006c4677c27c89403941
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2018-09-30 16:43:04 +0200

    added winner declaration

--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ class Board extends React.Component {
     handleClick(i){
         const squares = this.state.squares.slice();
 
-        // prevent double click on square
-        if(squares[i]){
+        // prevent double click on square and further clicking after a win
+        if(squares[i] || calculateWinner(squares)) {
             return;
         }
 
@@ -46,28 +46,35 @@ class Board extends React.Component {
     }
 
     render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner){
+            status = 'Winner: ' + (!this.state.xIsNext ? 'X' : 'O');
+        }
+        else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
-    return (
-        <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-        </div>
-        </div>
-    );
+        return (
+            <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+            </div>
+            </div>
+        );
     }
 }
 
@@ -93,3 +100,27 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+    // winning combinations
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for(let i = 0; i < lines.length; i++){
+        const a = lines[i][0];
+        const b = lines[i][1];
+        const c = lines[i][2];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a];
+        }
+    }
+    return null;
+
+}
