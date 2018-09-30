2ca1ba5807d30fc46bee54b8153c14cdb97acfec
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2018-09-30 16:32:34 +0200

    added taking turns functionality

--- src/index.js
+++ src/index.js
@@ -18,12 +18,22 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares : Array(9).fill(null),
+            xIsNext: true,
         };
     }
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+
+        // prevent double click on square
+        if(squares[i]){
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -36,7 +46,7 @@ class Board extends React.Component {
     }
 
     render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
         <div>
